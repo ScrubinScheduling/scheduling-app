2ec7e3e5a3943891354f5f493619c32b6d6fcd1e
kararAcode <63989704+kararAcode@users.noreply.github.com>
2025-10-13 11:59:31 -0600

    test api deploy job

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import { requireSession, ClerkExpressWithAuth } from '@clerk/express';
+// import { requireSession, ClerkExpressWithAuth } from '@clerk/express';
 import { prisma } from './db';
 
 const app = express();
@@ -17,14 +17,17 @@ app.get('/health', (_req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.get('/me', ClerkExpressWithAuth(), requireSession(), (req, res) => {
-  res.json({ userId: req.auth?.userId, sessionId: req.auth?.sessionId });
-});
+app.get('/test-endpoint', (req, res) => {
+  res.json({messaage: "App has been deployed"});
+}) 
+// app.get('/me', ClerkExpressWithAuth(), requireSession(), (req, res) => {
+//   res.json({ userId: req.auth?.userId, sessionId: req.auth?.sessionId });
+// });
 
-app.get('/users', ClerkExpressWithAuth(), requireSession(), async (_req, res) => {
-  const users = await prisma.user.findMany({ take: 10 });
-  res.json(users);
-});
+// app.get('/users', ClerkExpressWithAuth(), requireSession(), async (_req, res) => {
+//   const users = await prisma.user.findMany({ take: 10 });
+//   res.json(users);
+// });
 
 const port = process.env.PORT ? Number(process.env.PORT) : 4000;
 app.listen(port, () => {
