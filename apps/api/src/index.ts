cbcf7e7ee37583363d962dab738a6aa86783aa36
kararAcode <63989704+kararAcode@users.noreply.github.com>
2025-10-13 12:21:14 -0600

    fix typo with pnpm install

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -18,7 +18,7 @@ app.get('/health', (_req, res) => {
 });
 
 app.get('/test-endpoint', (req, res) => {
-  res.json({messaage: "App has been deployed v2"});
+  res.json({messaage: "App has been deployed v3"});
 }) 
 // app.get('/me', ClerkExpressWithAuth(), requireSession(), (req, res) => {
 //   res.json({ userId: req.auth?.userId, sessionId: req.auth?.sessionId });
