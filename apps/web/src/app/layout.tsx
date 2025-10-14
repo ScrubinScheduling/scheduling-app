d13aef3cf57e2ffe5a023b7f5702022401da62f1
kararAcode <63989704+kararAcode@users.noreply.github.com>
2025-10-14 03:13:36 -0600

    hardcode publishableKey for web

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+  const publishableKey = "pk_test_ZXhjaXRpbmctcGFycm90LTQxLmNsZXJrLmFjY291bnRzLmRldiQ";
   return (
     <html lang="en">
       <body
