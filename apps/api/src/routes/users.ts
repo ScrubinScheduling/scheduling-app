d6dd0485b1abe122249cfbfb5750303ec52cc3be
Talha Hassan <qzl241@usask.ca>
2025-11-17 12:06:58 -0600

    add get user by clerk id in users

--- apps/api/src/routes/users.ts
+++ apps/api/src/routes/users.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { prisma } from '../db';
+import { getUserByClerkId } from '../controllers/userController';
 
 const router = express.Router();
 
@@ -27,6 +28,8 @@ router.delete('/:id', async (req, res) => {
 	// TODO: Implement delete user
 	res.status(501).json({ error: 'Not implemented' });
 });
+
+router.get('/by-clerk/:clerkId', getUserByClerkId);
 
 export default router;
 
